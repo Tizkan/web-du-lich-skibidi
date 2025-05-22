c6cb3d064c6c0381f9a38fea28f54a66efb6f9f9
Tizkann <votrongtin87@gmail.com>
2025-05-22 11:58:19 +0700

    Delete Nhóm 6 quảng bá du lịch directory

--- Nhóm 6 quảng bá du lịch/php/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Xóa tất cả các biến phiên
-session_destroy(); // Hủy phiên
-header("Location: Loginn.php"); // Chuyển hướng về trang đăng nhập
-exit();
-?>
