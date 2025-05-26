3ae09ffbbbdffe53cf4eb33f3a9be1d57f382306
Tizkann <votrongtin87@gmail.com>
2025-05-26 17:15:03 +0700

    Update chi-tiet-sapa.php

--- Nhóm 6 quảng bá du lịch/chi-tiet-sapa.php
+++ Nhóm 6 quảng bá du lịch/chi-tiet-sapa.php
@@ -142,7 +142,44 @@ $result = mysqli_query($conn, $query);
                     <a href="?page=<?php echo $currentPage + 1; ?>">Trang tiếp theo</a>
                 <?php endif; ?>
             </div>
-        </body>
         </div>
     </section>
     <!-- Footer -->
+     <section class="footer">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <style tybe="text/css">
+            .fa-facebook{  margin-bottom: 5px;
+                font-size: 35px;
+            color: blue}
+            .fa-instagram{ margin-bottom: 5px;
+                font-size: 35px;
+            color: pink}
+            .fa-telegram{ margin-bottom: 5px;
+                font-size: 35px;
+            color: PaleTurquoise}
+            .fa-tiktok{ margin-bottom: 5px;
+                font-size: 35px;
+            color: white}
+    </style>   
+            <div class="row">
+                <div class="boxcenter2">
+                    <div class="boxfooter1"> 
+                        <h4>Liên hệ:</h4>
+                        <div class="add"><a href="https://www.facebook.com/tuoitreitqnu?locale=vi_VN" target="_blank"> <i class="fa-brands fa-facebook"></i></a></div>
+                          <div class="add"><a href="https://www.instagram.com/explore/locations/574687296282392/qnu-truong-ai-hoc-quy-nhon/" target="_blank"> <i class="fa-brands fa-instagram"></i></a></div>                        
+                        <div class="add"> <a href="https://www.tiktok.com/@itqnu1992" target="_blank"> <i class="fa-brands fa-tiktok"></i></a></div>
+                    </div>
+                    <div class="boxfooter2">
+                        <h4>Địa chỉ:</h4>
+                        <div class="add">Địa chỉ 1: 70 An Dương Vương, Quy Nhơn, Bình Định, Việt Nam</div>
+                        <div class="add">ĐỊa chỉ 2: Khoa công nghệ thông tin đại học quy nhơn</div>
+                    </div>
+                    <div class="boxfooter3">
+                        <h4>Đối tác liên kết:</h4>
+              <div class="add"><a href="https://www.agoda.com/search?city=4682&site_id=1922896&tag=badcebe4-e8d7-4f9f-a1ba-4e2e9b3cac9f&gad_source=1&gad_campaignid=21041750858&gbraid=0AAAAAD-GdVn4e-s6_hnP9_SooeuaLV_MM&device=c&network=g&adid=702728244461&rand=13488154135053900020&expid=&adpos=&aud=kwd-2263450249162&gclid=Cj0KCQjw_8rBBhCFARIsAJrc9yCfM-vFlJHnQgdhdqHwCRv6av40joAC1uuHxaxa7uVfwtBcKISv61waAsvlEALw_wcB&pslc=1&ds=Mq0EFBBJtst0%2BnJ2" target="_blank"><img src="img/agoda.png" alt="Agoda" style="height:40px; margin-top:8px;"></a></div>
+              <div class="add"><a href="https://jtexpress.vn/vi" target="_blank"><img src="img/JT.png" alt="JT" style="height:40px; margin-top:8px;"></a></div> 
+            <div class="add"><a href="https://www.momo.vn/" target="_blank"><img src="img/MM.png" alt="MM" style="height:40px; margin-top:8px;"></a>
+    </div>
+    </section>
+</body>
+</html>
